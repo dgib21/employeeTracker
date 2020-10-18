e666a601785ada09f2f1077d568014d6b3d274fd
Devin Gibson <dgib21@hotmail.com>
2020-10-18 10:42:39 -0500

    mapped out program sequence logic

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO
+  department (department_name)
+VALUES
+  ('Engineering'),
+  ('Finance'),
+  ('Sales'),
+  ('Legal');
+
+
+INSERT INTO
+  role (title, salary, department_id)
+VALUES
+  ('Software Engineer', 50000, 1),
+  ('Lead Engineer', 40000, 1),
+  ('Accountant', 30000, 2),
+  ('Finance Director', 20000, 2),
+  ('Salesman', 10000, 3),
+  ('Sales Lead', 5000, 3),
+  ('Lawyer', 2000, 4),
+  ('Legal Team Lead', 75000, 4);
+
+
+INSERT INTO
+  employee (first_name, last_name, role_id)
+VALUES
+  ('Softy', 'TheCoder', 1),
+  ('Tommy', 'TheTech', 2),
+  ('Money', 'May', 3),
+  ('Dollars', 'Dawn', 4),
+  ('Lead', 'Gen', 5, 6),
+  ('Close', 'Chan', 6),
+  ('Court', 'Chris', 7),
+  ('Judge', 'Jeff', 8);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const mysql = require('mysql2');
+const mysql2 = require('mysql2');
+const inquirer = require("inquirer");
 
-const connection = mysql.createConnection({
+
+//connect to DB 
+const connection = mysql2.createConnection({
   host: 'localhost',
   port: 3306,
   // Your MySQL username
@@ -9,3 +12,113 @@ const connection = mysql.createConnection({
   password: 'Family0521@',
   database: 'ice_creamDB'
 });
+
+//connect to the DB and if an error log it
+connection.connect(err => {
+    if (err) throw err;
+
+    console.log(
+        `====================================================================================`
+      );
+      console.log("\n WELCOME TO YOUR EMPLOYEE MANAGEMENT SYSTEM \n");
+      console.log("connected as id " + connection.threadId + "\n");
+      console.log(
+        `====================================================================================`
+      );    mainMenu();
+  });
+
+//function for main menu that will ask user questions
+function mainMenu() {
+
+    // Prompting user to choose an option 
+    inquirer
+      .prompt({
+        name: "main",
+        type: "list",
+        message: "MAIN MENU",
+        choices: [
+          "View all Departments",
+          "View all roles",
+          "View all Employees",
+          "Add department",
+          "Add a Role",
+          "Add Employee",
+          "Update employee role",
+          "Exit",
+        ],
+      })
+      .then((response) => {
+        // Switch case depending on user option
+        switch (response.main) {
+          case "View all departments":
+            viewAllDepartments();
+            break;
+          case "View all roles":
+            viewAllRoles();
+            break;
+          case "View all employees":
+            viewAllEmployees();
+            break;
+            case "Add role":
+            addEmployeeRole();
+            break;
+          case "Add employee":
+            addEmployee();
+            break;
+          case "Update employee role":
+            updateEmployeeRole();
+            break;
+          case "Exit":
+            connection.end();
+            break;
+          default:
+            mainMenu();
+        }
+      });
+  }
+
+
+//function to display all departments to user
+
+function viewAllDepartments() {
+    console.log("departments"); 
+
+
+}
+
+
+//function to display all roles 
+
+function viewAllRoles() {
+    console.log("Roles"); 
+
+
+}
+
+
+//function to add a department 
+function viewAllEmployees() {
+    console.log("All Employees"); 
+
+
+}
+
+//function to add a role 
+
+function addEmployeeRole() {
+    console.log("employee role aDDED"); 
+
+
+}
+
+//function to add an employee
+function addEmployee() {
+    console.log("Employee added"); 
+
+}
+
+//function to update an employee role 
+function updateEmployeeRole() {
+    console.log("employee role updated"); 
+
+}
